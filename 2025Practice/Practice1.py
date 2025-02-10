864d7bb94d23c8d7b32c9d63af2433e498df9c85
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-02-09 18:50:27 -0800

    Create Practice1.py

---
+++ 2025Practice/Practice1.py
@@ -0,0 +1,14 @@
+# 2/9/2025
+"""Restarting daily Python practice.
+This is a way to create strings. If they are not assigned, they are treated as comments."""
+x = 333 
+y = "Oleg"
+print(x,'and',y)
+'''In Python, variables don't require a specific type to be declared, and their type can change even after they've been assigned a value.'''
+#Casting allows you to explicitly define the data type of a variable.
+m = str(555) # '555'
+n = int(34) # 34
+o = float(342)  # 342
+
+# to get the datatype of the variable: 
+print('m is a:', type(m), 'n is a', type(n), 'o is a', type(o))
