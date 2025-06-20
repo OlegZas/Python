3a40ca8698eb3f74178e751d6b05c36f4eac3b46
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-06-19 20:10:25 -0700

    Update Lab01_OZ.py

--- PythonClass/Python119/Lab01_OZ.py
+++ PythonClass/Python119/Lab01_OZ.py
@@ -21,6 +21,7 @@ else:
         print("%s testing and trying nested loop in condtional statment" % (name))
         y +=1 # REMEMBER:  instead y = y+1 just do y += 1
 
+
 #6/18/25 Yo voy a atemptar a continuar mi practica de Python. En mismo tiempo, a veces, voy a aprender y practicar espanol. 
 # Python shortcuts 
 
@@ -119,3 +120,72 @@ print(lst)
 # but they all sort based on the first number or character 
 lst.sort(key=lambda x: x[1], reverse=True) # lambda is a one line funciton , x is a parameter returning x[1] index element 
 print(lst)
+# ******************************************************************************************************************************
+#6/19/2025 DATA TYPES : 
+# ******************************************************************************************************************************
+#datatypes define the type of value that variable holds. 
+#Integers and floats 
+print (type(-12)) # type is used to print the data type of the value 
+
+print(type (434 + 11.23))
+
+#Boolean 
+print(type (1 > 5))
+
+#Strings 
+multiline = """ # multi line 
+La confianza es muy importante.
+Pienso que para la obtener, 
+neceista completar objectivos 
+y entender 
+y saber su characteristicas positivas. """
+
+print(multiline)
+#negating double quotes 
+Alturaancho = """ quote's negation "is here" """
+print(Alturaancho, altura, "printing %s" % (ancho))
+
+callado = 'quiet'
+print(callado[:5])
+print(callado * 3)
+
+# LISTS - are changable objects 
+la_lista = [53,'Que Opina','How about that']
+print(la_lista)
+la_lista.append('merendar - to snack')
+print(la_lista)
+
+nested = ['recto','straight', ['permancer','to stay'], 'reir-to lough']
+print(nested[2])#index two has a nested list element 
+
+#tuple - also indexed; can't change or add anything to a tuple. 
+### used when data will never change 
+
+tuple_scoops = (1,2,3,4,6) 
+print(tuple_scoops)
+
+#sets 
+extrano = {1,5,7}
+print(list(zip(extrano, name)))
+
+surge = {15,34,53,53}
+emerge = {343,15,42,53}
+print(surge | emerge)#get the distinct values between the two sets 
+print(surge - emerge) # values in the lists that don't match 
+print(surge ^ emerge) #completely unique values between the two sets 
+
+#dictionaries 
+#key/value pair 
+acercarseToapproach = {'cruzar': 'cross','asegurar':'to assure', 'pegar':['to hit', 'odiar','to hate'] }
+print(type(acercarseToapproach))
+print(acercarseToapproach.values())
+print(acercarseToapproach.items())#prints pairs 
+print(acercarseToapproach['cruzar'])
+acercarseToapproach['cruzar'] = acercarseToapproach['asegurar'] # updating dictionary value 
+print(acercarseToapproach['cruzar'])
+acercarseToapproach.update({'cruzar': 'cross','asegurar':'to assure', 'pegar':['to hit', 'odiar','to hate'] })
+print(f"casting into a string {acercarseToapproach}")
+print(type(acercarseToapproach))
+print(type(f"casting into a string {acercarseToapproach}"))
+del acercarseToapproach['asegurar'] # deleting a key and value from the dictionary 
+print(acercarseToapproach)
