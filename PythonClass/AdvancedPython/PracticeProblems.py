8c653ef4a7320326b482f782c1c4884951c514ad
oz <115661636+OlegZas@users.noreply.github.com>
2026-03-03 18:24:21 -0800

    Update PracticeProblems.py

--- PythonClass/AdvancedPython/PracticeProblems.py
+++ PythonClass/AdvancedPython/PracticeProblems.py
@@ -7,3 +7,13 @@ class Solution:
                 mine.append(i)  
         nums[:] = mine  #[:] - refers to the entire contents of the list on the right from start to finish 
         return len(nums)
+
+#2. Given an array of integers and a target number, return the indexes of two numbers that add up to the target number. \
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numbers = {}
+        for i, num in enumerate(nums):
+            residual = target - num
+            if residual in numbers:
+                return [i, numbers[residual]]
+            numbers [num] = i
