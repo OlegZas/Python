de15d9fa735f07f423ec51cc17870adc334acfd4
oz <115661636+OlegZas@users.noreply.github.com>
2026-03-03 16:08:28 -0800

    Create PracticeProblems.py

---
+++ PythonClass/AdvancedPython/PracticeProblems.py
@@ -0,0 +1,9 @@
+# Identifying and removing the duplicates from the list in place 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        mine = []  
+        for i in nums:
+            if i not in mine:   
+                mine.append(i)  
+        nums[:] = mine  #[:] - refers to the entire contents of the list on the right from start to finish 
+        return len(nums)
